34825a08a834f68611f1f5a538fd4577d22b70ef
Practice <gurungriwaaz7@gmail.com>
2024-11-30 01:10:21 -0500

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Navbar from "./components/navBar";
 import About from "./components/about";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div>
       <Navbar />
       <About />
+      <Footer />
     </div>
   );
 }
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -60,7 +60,7 @@ const About = () => {
                   className="text-2xl font-bold text-transparent bg-clip-text 
                 bg-gradient-to-r from-green-400 to-blue-500"
                 >
-                  3+
+                  4+
                 </h3>
                 <p>Years Experience</p>
               </div>
@@ -69,7 +69,7 @@ const About = () => {
                   className="text-2xl font-bold text-transparent bg-clip-text 
                 bg-gradient-to-r from-green-400 to-blue-500"
                 >
-                  50+
+                  10+
                 </h3>
                 <p>Projects Completed</p>
               </div>
